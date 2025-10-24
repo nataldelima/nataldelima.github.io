e7c788e942c48db3b3db60078832997094b2c1d7
Natal Lima <nataldelima@gmail.com>
2025-10-24 15:07:50 -0400

    Remoção de codigo extra

--- functions.js
+++ functions.js
@@ -54,54 +54,7 @@ document.addEventListener('DOMContentLoaded', () => {
     updateButtons(); // Initialize button visibility
 });
 
-
-/*
-
-const phoneNumber = "5567991609897";
-function detectOS() {
-    const userAgent = navigator.userAgent;
-    const platform = navigator.platform;
-
-    if (/Android|iPhone|iPad|iPod|Windows/.test(userAgent)) {
-        return "mobileOrWindows";
-    }
-    return "other";
-}
-
-function updateWhatsAppLink() {
-    const whatsappLink = document.getElementById("whatsapp");
-    const message = `Olá! 👋 
-    
-    🖥️ Gostaria de informações para fazer um site. 📱
-    
-    Pode me ajudar?`;
-    const encodedMessage = encodeURIComponent(message);
-    const baseURL = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodedMessage}`;
-    const webURL = `https://web.whatsapp.com/send?phone=${phoneNumber}&text=${encodedMessage}`;
-
-    if (detectOS() === "mobileOrWindows") {
-        whatsappLink.href = baseURL;
-    } else {
-        whatsappLink.href = webURL;
-    }
-}
-
-// Executa a função ao carregar a página
-window.onload = updateWhatsAppLink;
-
-
-function sendWhatsAppMessage() {
-    const nome = document.getElementById('nome').value;
-    const email = document.getElementById('email').value;
-    const fone = document.getElementById('fone').value;
-    const mensagem = document.getElementById('mensagem').value;
-    const whatsappMessage = `*Nome:* ${nome}%0A*Email:* ${email}%0A*Telefone:* ${fone}%0A*Mensagem:* ${mensagem}`;
-    const app = detectOS() === "mobileOrWindows" ? 'api' : 'web';
-    const whatsappUrl = `https://${app}.whatsapp.com/send?phone=${phoneNumber}&text=${whatsappMessage}`;
-    window.open(whatsappUrl, '_blank');
-}
-
-*/
+//Identificar o sistema operacional
 
 function detectOS() {
     const userAgent = navigator.userAgent;
